b8e40f102e0f301afaa65e8ad5d964e829ef5783
Femi6002 <oluwafemiolatunji6002@gmail.com>
2025-07-09 10:23:26 +0100

    Auth model First with postgres --Login and register primarily done

--- src/main/java/com/example/campus_navigation_system_and_lecturer_availability_api/modules/auth/util/JwtUtil.java
+++ src/main/java/com/example/campus_navigation_system_and_lecturer_availability_api/modules/auth/util/JwtUtil.java
@@ -4,7 +4,6 @@ import com.example.campus_navigation_system_and_lecturer_availability_api.module
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -16,14 +15,12 @@ public class JwtUtil {
 
     private final String jwtSecret = "5a8d6f89b9e249e3b8a1f78e7e9db53e3faba23b529dbdf6c9fead05f4b92d1a68a91e1472b19118a6795487428f04d4=";
 
-    @Value("${jwt.expiration}")
-    private long jwtExpiration;
-
     // convert your secret string to a secure key object
     Key key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
 
     public String generateToken(User user) {
         Date now = new Date();
+        long jwtExpiration = 86400000;
         Date expiryDate = new Date(now.getTime() + jwtExpiration);
 
         return Jwts.builder()
